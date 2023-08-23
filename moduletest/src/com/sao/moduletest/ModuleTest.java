b3e6963256ab147035fa3da8029e609282d6e184
saozd <saozdemir1991@gmail.com>
2023-08-24 00:52:36 +0300

    Initial commit

---
+++ moduletest/src/com/sao/moduletest/ModuleTest.java
@@ -0,0 +1,12 @@
+package com.sao.moduletest;
+
+/**
+ * @author saozd
+ * @project com.sao.moduletest javaworkspace
+ * 24.08.2023 Ağu 2023
+ */
+public class ModuleTest {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
